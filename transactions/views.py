643bd8fb6b44667a932c7fe9b1a002d4638c9f03
NaianeReis27 <email.annereis@gmail.com>
2023-01-27 23:30:13 -0300

    feat: create gitignore

--- transactions/views.py
+++ transactions/views.py
@@ -1,13 +1,13 @@
 from .serializers import TransactionsSerializers
 from .models import Transactions
-from django.shortcuts import render , get_list_or_404
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 import datetime
 
+
 def formTransactions(request):
     transactions = Transactions.objects.all()
-    
-    
+
     if request.method == "POST":
 
         if request.FILES['document'].content_type != "text/plain":
@@ -16,24 +16,29 @@ def formTransactions(request):
         file = request.FILES['document'].readlines()
 
         for index, ele in enumerate(file):
-            print(ele,index)
+            print(ele, index)
             try:
                 data_decoded = ele.decode(
-                        'utf-8').replace("\r", "").replace("\n", "")
+                    'utf-8').replace("\r", "").replace("\n", "")
                 date = datetime.date(int(data_decoded[1: 5]), int(
-                        data_decoded[5: 7]), int(data_decoded[7: 9]))
+                    data_decoded[5: 7]), int(data_decoded[7: 9]))
                 time = datetime.time(int(data_decoded[42: 44]), int(
-                        data_decoded[44: 46]), int(data_decoded[46: 48]))
+                    data_decoded[44: 46]), int(data_decoded[46: 48]))
+
+                if data_decoded[0: 1] == '2' or data_decoded[0: 1] == '3' or data_decoded[0: 1] == '9':
+                    value = -int(data_decoded[10: 19])/100
+                else:
+                    value = int(data_decoded[10: 19])/100
 
                 data = {
-                        'type':  data_decoded[0: 1],
-                        'data':  date,
-                        'value': round(int(data_decoded[10: 19]), 2),
-                        'cpf':   data_decoded[19: 30],
-                        'card':  data_decoded[30: 42],
-                        'hour':  time,
-                        'owner': data_decoded[48: 62],
-                        'store': data_decoded[62: 81]
+                    'type':  data_decoded[0: 1],
+                    'data':  date,
+                    'value': round(value, 2),
+                    'cpf':   data_decoded[19: 30],
+                    'card':  data_decoded[30: 42],
+                    'hour':  time,
+                    'owner': data_decoded[48: 62],
+                    'store': data_decoded[62: 81]
                 }
 
             except:
@@ -44,14 +49,15 @@ def formTransactions(request):
             if serializer.is_valid():
                 serializer.save()
 
-    return render(request, 'transactions/addFile.html', {'transactions': transactions})
+    return render(request, 'transactions/home.html', {'transactions': transactions})
+
 
 def filter(request, store):
-   
+
     if request.method == "GET":
-        transactions = get_list_or_404(Transactions, store = store)
+        transactions = get_list_or_404(Transactions, store=store)
         soma = 0
-        for transaction in transactions :
+        for transaction in transactions:
             soma = transaction.value + soma
 
-    return render(request, 'transactions/list.html', {'transactions': transactions, 'soma': soma})
+    return render(request, 'transactions/store.html', {'transactions': transactions, 'soma': soma})
